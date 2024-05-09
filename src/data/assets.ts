1d82b184481dd1a8d1ce7bf31e1755d735e12237
Abdulsamad Ayoade <ayoadesamad@gmail.com>
2024-05-09 16:28:22 +0100

    chore: add assets data

---
+++ src/data/assets.ts
@@ -0,0 +1,94 @@
+import type { TInventory } from "../modules/inventory/types";
+
+export const assets: TInventory[] = [
+  {
+    id: "RC-81247932",
+    type: "capsule",
+    status: "active",
+    dateAdded: new Date("2021-09-05"),
+  },
+  {
+    id: "RC-81247933",
+    type: "energy-box",
+    status: "inactive",
+    dateAdded: new Date("2021-09-08"),
+  },
+  {
+    id: "RC-81247934",
+    type: "big-energy",
+    status: "rented-out",
+    dateAdded: new Date("2021-12-05"),
+  },
+  {
+    id: "RC-81247935",
+    type: "bike",
+    status: "active",
+    dateAdded: new Date("2021-06-05"),
+  },
+  {
+    id: "RC-81247936",
+    type: "van",
+    status: "inactive",
+    dateAdded: new Date("2021-09-05"),
+  },
+  {
+    id: "RC-81247932",
+    type: "capsule",
+    status: "active",
+    dateAdded: new Date("2021-09-05"),
+  },
+  {
+    id: "RC-81247933",
+    type: "energy-box",
+    status: "inactive",
+    dateAdded: new Date("2021-09-08"),
+  },
+  {
+    id: "RC-81247934",
+    type: "big-energy",
+    status: "returned",
+    dateAdded: new Date("2021-12-05"),
+  },
+  {
+    id: "RC-81247935",
+    type: "bike",
+    status: "active",
+    dateAdded: new Date("2021-06-05"),
+  },
+  {
+    id: "RC-81247936",
+    type: "van",
+    status: "inactive",
+    dateAdded: new Date("2021-09-05"),
+  },
+  {
+    id: "RC-81247932",
+    type: "capsule",
+    status: "active",
+    dateAdded: new Date("2021-09-05"),
+  },
+  {
+    id: "RC-81247933",
+    type: "energy-box",
+    status: "inactive",
+    dateAdded: new Date("2021-09-08"),
+  },
+  {
+    id: "RC-81247934",
+    type: "big-energy",
+    status: "rented-out",
+    dateAdded: new Date("2021-12-05"),
+  },
+  {
+    id: "RC-81247935",
+    type: "bike",
+    status: "active",
+    dateAdded: new Date("2021-06-05"),
+  },
+  {
+    id: "RC-81247936",
+    type: "van",
+    status: "inactive",
+    dateAdded: new Date("2021-09-05"),
+  },
+];
