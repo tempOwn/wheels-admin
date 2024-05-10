696acd56a9a401197c90c9318be928fd6eb97597
DannyK05 <olubanjok@gmail.com>
2024-05-10 02:06:55 +0100

    feat: implemented the top ambassador card, activity log and overall dashboard styling

---
+++ src/components/icons/ChevronLeft.tsx
@@ -0,0 +1,9 @@
+export default function ChevronLeft (){
+    return(
+        <svg width="16" height="17" viewBox="0 0 16 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <g opacity="0.3">
+        <path d="M10 12.5L6 8.5L10 4.5" stroke="#00283D" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+        </g>
+        </svg>
+    )
+}
