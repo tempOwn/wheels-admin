c1755ea960cf6e7e0191cb0df2a493e9540ae784
DannyK05 <olubanjok@gmail.com>
2024-05-10 01:47:02 +0100

    feat: implemented the rental chart

---
+++ src/components/icons/SortIcon.tsx
@@ -0,0 +1,8 @@
+export default function SortIcon(){
+    return(
+        <svg width="18" height="13" viewBox="0 0 18 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M4 6.5H14M1.5 1.5H16.5M6.5 11.5H11.5" stroke="#55707E" strokeWidth="1.66667" strokeLinecap="round" strokeLinejoin="round"/>
+        </svg>
+
+    )
+}
