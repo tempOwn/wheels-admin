849bb6f01fc49a1346fa525ae1917e9deafe3720
DannyK05 <olubanjok@gmail.com>
2024-05-10 02:18:12 +0100

    Revert "feat: implemented the activity log, rental chart and top sales card"
    This reverts commit 7bd4fc1755a6401c23e310c3f6b1ea986f3ad9de.

--- src/modules/dashboard/index.tsx
+++ src/modules/dashboard/index.tsx
@@ -1,8 +1,6 @@
 import BatteryIcon from "@/src/components/icons/BatteryIcon";
 import CapsulesIcon from "@/src/components/icons/CapsulesIcon";
 import InfoIcon from "@/src/components/icons/InfoIcon";
-import RentalGraph from "./components/RentalGraph";
-import Image from "next/image";
 
 type Tanalytics = {
   icon: React.ReactNode | string;
@@ -34,60 +32,8 @@ const analytics :Tanalytics[] = [
     value: 46
   }
 ]
-const activityLogs = [
-  {
-    image: "",
-    title: "Customer Registration",
-    message: "Amb. Edmund Doyle added a new customer, Your approval is needed.",
-    time: "58 minutes ago"
-  },
-  {
-    image: "",
-    title: "Customer Registration",
-    message: "Amb. Edmund Doyle added a new customer, Your approval is needed.",
-    time: "58 minutes ago"
-  },
-  {
-    image: "",
-    title: "Customer Registration",
-    message: "Amb. Edmund Doyle added a new customer, Your approval is needed.",
-    time: "58 minutes ago"
-  },
-  {
-    image: "",
-    title: "Customer Registration",
-    message: "Amb. Edmund Doyle added a new customer, Your approval is needed.",
-    time: "58 minutes ago"
-  },
-  {
-    image: "",
-    title: "Customer Registration",
-    message: "Amb. Edmund Doyle added a new customer, Your approval is needed.",
-    time: "58 minutes ago"
-  }
-]
-const topSales = [
-  {
-    id: 1,
-    name: "Reeddi Capsules",
-    sales: 345,
-    image: ""
-  },
-  {
-    id: 2,
-    name: "EnergyBOX",
-    sales: 134,
-    image: ""
-  },
-  {
-    id: 3,
-    name: "BigEnergy",
-    sales: 64,
-    image: ""
-  }
-]
 export default function Dashboard() {
-  const currentDate= new Date("2023-10-25")
+  const currentDate= new Date("Thurs 25th October,2023")
 
   return(
     <div>
@@ -97,17 +43,17 @@ export default function Dashboard() {
           <span>See detailed analytics</span>
         </div>
         <span className="border">
-          {/* {currentDate} */}
+          {currentDate}
         </span>
       </div>
       <div className="flex items-center space-x-5">
         <div className=" w-2/3 space-y-5">
           <div className="flex items-center space-x-5">
-            <div className="flex items-center space-x-5">
+            <div className="flex flex-wrap items-center space-x-5">
               {
                 analytics ? 
                 analytics.map(({category, icon, value, change}:Tanalytics) =>
-                  <div key={category} className="border bg-white w-1/2 rounded-sm">
+                  <div key={category} className="border w-1/2 rounded-sm">
                     <div className="flex items-center justify-between p-4">
                       <span className={`p-1 rounded-full ${category === "System transferred" ? "bg-wheels-primary" :
                        category === "Rentals" ? "bg-[#10B981]":
@@ -139,7 +85,7 @@ export default function Dashboard() {
               }
             </div>
 
-            <div className=" bg-white flex flex-col items-center border rounded-sm space-x-3 ">
+            <div className="flex flex-col items-center border rounded-sm space-x-3 ">
               <div className="flex items-center justify-betwwen">
                 <h2>Top Performers</h2>
                 <div>
@@ -159,60 +105,18 @@ export default function Dashboard() {
             </div>
           </div>
 
-          <div className="w-full">
-              <RentalGraph/>
+          <div>
+
           </div>
         </div>
 
         <div>
-          <div className="bg-white">
-            <div className="flex items-center justify-between">
-              <p>Activity Log</p>
-              <button>
-                View all
-              </button>
-            </div>
-            <div>
-              { activityLogs.map(({image, title, time, message}) =>
-                <div key={title} className="flex items-center border-t space-x-2">
-                  <Image src={image} alt={title}/>
-                  <div className="flex flex-col items-center space-y-2">
-                    <div className="flex items-center space-x-1">
-                      <p>{title}</p>
-                      <span>{time}</span>
-                    </div>
-                    <p>{message}</p>
-                  </div>
-                </div>
-              )
-              }
-            </div>
+          <div>
 
           </div>
           
           <div>
-              <p>Top Selling Items</p>
-              <div>
-                {
-                  topSales.map(({name, id, image, sales})=>
-                    <div key={id} className="flex items-center space-x-2">
-                      <span>{id}</span>
-                      <Image src={image} alt={name}/>
-                      <div key={id}>
-                        <p>{name}</p>
-                        <div>
-                          <div className="h-[10px] rounded-lg">
-                            <div className=" h-full bg-wheels-primary rounded-lg">
-                              
-                            </div>
-                          </div>
-                          <span>{sales}</span>
-                        </div>
-                      </div>
-                    </div>
-                  )
-                }
-              </div>
+
           </div>
         </div>
       </div>
