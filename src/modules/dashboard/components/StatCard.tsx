04cc36402d30e9400eedcb8447684e447590dde8
Abdulsamad Ayoade <ayoadesamad@gmail.com>
2024-06-05 12:45:43 +0100

    refactor: refactored

---
+++ src/modules/dashboard/components/StatCard.tsx
@@ -0,0 +1,56 @@
+import InfoIcon from "@/src/components/icons/InfoIcon";
+import SystemsIcon from "@/src/components/icons/SystemsIcon";
+import { cn } from "@/src/lib/utils";
+
+export type StatsCardProps = {
+  icon?: React.ReactNode;
+  iconBackgroundClass: string;
+  title: string;
+  statValue: number;
+  description?: JSX.Element | string;
+};
+
+export default function StatCard({
+  icon,
+  iconBackgroundClass,
+  description,
+  statValue,
+  title,
+}: StatsCardProps) {
+  return (
+    <div className="border-wheels-border-2 flex flex-col justify-between rounded-lg border bg-white p-4">
+      <div className="mb-10 flex items-center justify-between space-x-2 lg:mb-5">
+        <div
+          className={cn(
+            "flex h-6 w-6 items-center justify-center rounded-full",
+            iconBackgroundClass,
+          )}
+        >
+          {icon ? icon : <SystemsIcon />}
+        </div>
+
+        {description && (
+          <>
+            {typeof description === "string" ? (
+              <p className="text-sm text-wheels-grey">{description}</p>
+            ) : (
+              description
+            )}
+          </>
+        )}
+      </div>
+
+      <div className="">
+        <p className="mb-2 text-2xl font-bold text-wheels-primary sm:text-3xl md:text-4xl">
+          {statValue}
+        </p>
+        <div className="flex items-center justify-between space-x-2">
+          <span className="text-sm text-wheels-secondary">{title}</span>
+          <button>
+            <InfoIcon />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
