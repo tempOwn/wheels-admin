628ee0dde73437bb6c565865f12f3c5649782775
Abdulsamad Ayoade <ayoadesamad@gmail.com>
2024-06-24 11:31:11 +0100

    chore: refactor login component

--- src/modules/auth/login/Login.tsx
+++ src/modules/auth/login/Login.tsx
@@ -1,119 +1,111 @@
 "use client";
 import Image from "next/image";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
 import CoverImage from "@/public/assets/images/login-cover-image.png";
-import Link from "next/link";
-import { useState } from "react";
-import XIcon from "@/src/components/icons/XIcon";
+
+export const loginFormSchema = z.object({
+  email: z
+    .string()
+    .min(6, { message: "Required" })
+    .email("This is not a valid email."),
+  password: z.string().min(6, { message: "Required" }),
+});
 
 export default function Login() {
-  const [formData, setFormData] = useState({
-    userEmail: "",
-    userPassword: "",
+  const {
+    formState: { errors },
+    register,
+    ...form
+  } = useForm<z.infer<typeof loginFormSchema>>({
+    resolver: zodResolver(loginFormSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
   });
-  const [formError, setFormError] = useState("");
-  const [validation, setValidation] = useState(true);
-  const saveUserData = (e: any) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
-  const validateForm = (e: any) => {
-    e.preventDefault();
-    if (formData.userEmail.trim() === "") {
-      setValidation(false);
-      setFormError("Email field is empty");
-      console.log("click");
-    } else if (formData.userPassword.trim() === "") {
-      setValidation(false);
-      setFormError("Password field is empty");
-    } else if (formData.userPassword.length < 5) {
-      setValidation(false);
-      setFormError("Password should be more than 5 characters");
-    } else {
-      setValidation(true);
-    }
-    setTimeout(() => {
-      setFormError("");
-    }, 5000);
-  };
+
+  async function onSubmit(values: z.infer<typeof loginFormSchema>) {
+    console.log(values);
+  }
+
   return (
-    <div className="align-center flex h-[100vh] w-full">
-      <div className="hidden w-3/5 md:block lg:block">
+    <div className="align-center flex h-screen w-full">
+      <div className="hidden lg:block lg:w-1/2">
         <Image className="h-full w-full" src={CoverImage} alt="Cover Image" />
       </div>
-      <div className=" w-full px-4 py-8 md:w-2/5 lg:w-2/5 lg:px-10">
-        <div className="w-full">
-          {formError && (
-            <div className="absolute right-[10px] top-[20px] flex items-center justify-between rounded-sm bg-wheels-primary p-2 text-xs text-white shadow-lg lg:right-[60px] lg:w-1/3">
-              <p>{formError}</p>
-              <button onClick={() => setFormError("")}>
-                <span>
-                  <XIcon />
+
+      <div className="w-full bg-[#F1F5F8] px-5 py-10 lg:w-1/2 lg:py-16">
+        <div className="flex w-full flex-col items-center space-y-5">
+          <h1 className="flex items-end space-x-1 font-bold leading-none text-wheels-primary">
+            <span className="-mb-1 text-5xl lg:text-6xl">Wheels</span>
+            <span className="h-2.5 w-2.5 rounded-full bg-wheels-grey"></span>
+          </h1>
+
+          <p className="text-lg font-bold text-wheels-primary md:text-xl lg:text-2xl">
+            Sign in for free
+          </p>
+        </div>
+
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="mx-auto mt-10 flex w-full max-w-96 flex-col items-center justify-center">
+          <div className="flex w-full flex-col">
+            <label className="flex w-full flex-col items-start space-y-1">
+              <span className="text-sm text-wheels-grey">Email Address</span>
+              <input
+                {...register("email")}
+                className="w-full rounded-lg border border-wheels-border bg-white p-3 text-sm outline-none focus:border-wheels-primary"
+                type="email"
+                placeholder="Enter Email Address"
+              />
+              {errors.email && (
+                <span className="text-xs text-red-500">
+                  {errors.email.message}
                 </span>
-              </button>
-            </div>
-          )}
-          <div className="flex w-full flex-col items-center">
-            <div className="flex items-end space-x-2">
-              <h1 className="text-5xl font-bold text-wheels-primary md:text-5xl lg:text-7xl">
-                Wheels
-              </h1>
-              <sub className="rounded-full bg-wheels-grey px-2 py-2"></sub>
-            </div>
-            <h2 className="mt-5 text-xl font-bold text-wheels-primary lg:text-3xl">
-              Sign in for free
-            </h2>
+              )}
+            </label>
+
+            <label className="mb-2 mt-5 flex w-full flex-col items-start space-y-2">
+              <span className="text-sm text-wheels-grey">Password</span>
+              <input
+                {...register("password")}
+                className="w-full rounded-lg border border-wheels-border bg-white p-3 text-sm outline-none focus:border-wheels-primary"
+                type="password"
+                placeholder="Enter Password"
+              />
+              {errors.password && (
+                <span className="text-xs text-red-500">
+                  {errors.password.message}
+                </span>
+              )}
+            </label>
+
+            <button className="ml-auto text-sm text-wheels-grey">
+              Forgot Password ?
+            </button>
           </div>
 
-          <form
-            onSubmit={validateForm}
-            className=" flex w-full flex-col items-center justify-center px-2">
-            <div className="flex w-full flex-col items-end space-y-2 px-2">
-              <label className="flex w-full flex-col items-start space-y-2">
-                <span className="text-sm text-wheels-grey">Email Address</span>
-                <input
-                  className="w-full rounded-lg border px-2 py-4 text-sm "
-                  name="userEmail"
-                  type="email"
-                  onChange={saveUserData}
-                  placeholder="jonsnow@gmail.com"
-                />
-              </label>
-              <label className="flex w-full flex-col items-start space-y-2">
-                <span className="text-sm text-wheels-grey">Password</span>
-                <input
-                  className="w-full rounded-lg border px-2 py-4 text-sm "
-                  name="userPassword"
-                  onChange={saveUserData}
-                  type="password"
-                  placeholder="Password"
-                />
-              </label>
-              <button className="text-sm text-wheels-grey">
-                Forgot Password ?
-              </button>
-            </div>
-            <div className=" mt-10 flex w-full flex-col items-center space-y-4 text-wheels-grey">
-              <div className="text-sm">
-                <p className="text-center ">
-                  You acknowledge that you read and agree to our
-                </p>
-                <div className="text-center">
-                  <Link className="font-medium text-wheels-grey-3" href="">
-                    Terms of Service
-                  </Link>{" "}
-                  and our{" "}
-                  <Link className="font-medium text-wheels-grey-3" href="">
-                    Privacy Policy
-                  </Link>
-                </div>
-              </div>
-              <button
-                className={`${validation ? "bg-white text-wheels-primary" : "bg-wheels-primary text-white"} text-semibold w-full rounded-lg border-[1px] border-wheels-primary px-10 py-4`}
-                type="submit">
-                Sign in
-              </button>
-            </div>
-          </form>
-        </div>
+          <div className="mt-20 flex w-full flex-col items-center space-y-4 text-sm text-wheels-grey lg:space-y-6">
+            <p className="text-center">
+              You acknowledge that you read and agree to our
+              <span className="font-medium text-wheels-grey-3">
+                Terms of Service
+              </span>{" "}
+              and our{" "}
+              <span className="font-medium text-wheels-grey-3">
+                Privacy Policy
+              </span>
+            </p>
+
+            <button
+              className="text-semibold w-full rounded-lg border border-wheels-primary bg-white px-10 py-3 transition-all duration-200 ease-in-out hover:bg-wheels-primary hover:text-white"
+              type="submit">
+              Sign in
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   );
