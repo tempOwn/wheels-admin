5123efe417ebbc6392da2a9d6d025f938aa22f85
Abdulsamad Ayoade <ayoadesamad@gmail.com>
2024-07-12 11:06:34 +0100

    chore: refactor stats card

--- src/components/common/StatCard.tsx
+++ src/components/common/StatCard.tsx
@@ -5,6 +5,7 @@ type StatCardProps = {
   iconClass: string;
   value: number | string;
   description: string;
+  className?: string;
 };
 
 export default function StatCard({
@@ -12,9 +13,10 @@ export default function StatCard({
   iconClass,
   value,
   description,
+  className,
 }: StatCardProps) {
   return (
-    <div className="rounded-sm bg-white p-3 shadow-card">
+    <div className={cn("rounded-sm bg-white p-3 shadow-card", className)}>
       <div
         className={cn(
           "flex h-[22px] w-[22px] items-center justify-center rounded-full",
@@ -23,7 +25,7 @@ export default function StatCard({
         {icon}
       </div>
 
-      <p className="mb-1 mt-5 text-lg font-bold text-wheels-primary sm:text-xl md:text-2xl lg:mt-8 lg:text-3xl">
+      <p className="mb-1 mt-5 text-lg font-bold text-wheels-primary sm:text-xl md:text-2xl lg:mt-8 xl:text-3xl">
         {value}
       </p>
       <p className="text-xs text-black lg:text-sm">{description}</p>
