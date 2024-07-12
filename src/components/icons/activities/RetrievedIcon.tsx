33b1e89140b86337ee0513f7f3d720686d1f12d7
Abdulsamad Ayoade <ayoadesamad@gmail.com>
2024-07-12 10:53:08 +0100

    chore: add icons

---
+++ src/components/icons/activities/RetrievedIcon.tsx
@@ -0,0 +1,33 @@
+export default function RetrievedIcon() {
+  return (
+    <svg
+      width="32"
+      height="33"
+      viewBox="0 0 32 33"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg">
+      <ellipse
+        cx="15.7866"
+        cy="16.1255"
+        rx="15.7866"
+        ry="16"
+        fill="#00A0FF"
+        fillOpacity="0.2"
+      />
+      <path
+        d="M20.231 11.4678L11.3422 20.3566"
+        stroke="#0070B2"
+        strokeWidth="2.08696"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M20.231 20.3566H11.3422V11.4678"
+        stroke="#0070B2"
+        strokeWidth="2.08696"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
---
+++ src/components/icons/activities/SentBackIcon.tsx
@@ -0,0 +1,59 @@
+export default function SentBackIcon() {
+  return (
+    <svg
+      width="32"
+      height="33"
+      viewBox="0 0 32 33"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg">
+      <ellipse
+        cx="15.7867"
+        cy="16.2305"
+        rx="15.7867"
+        ry="16"
+        fill="#02C2CE"
+        fillOpacity="0.2"
+      />
+      <g clipPath="url(#clip0_473_13042)">
+        <path
+          d="M11.3422 25.7947L7.78662 22.2391L11.3422 18.6836"
+          stroke="#02C2CE"
+          strokeWidth="2.08696"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M23.7866 16.9062V18.684C23.7866 19.627 23.412 20.5314 22.7452 21.1982C22.0784 21.865 21.1741 22.2396 20.2311 22.2396H7.78662"
+          stroke="#02C2CE"
+          strokeWidth="2.08696"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M20.2311 6.23926L23.7866 9.79481L20.2311 13.3504"
+          stroke="#02C2CE"
+          strokeWidth="2.08696"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M7.78662 15.1278V13.35C7.78662 12.407 8.16122 11.5026 8.82802 10.8358C9.49482 10.169 10.3992 9.79443 11.3422 9.79443L23.7866 9.79443"
+          stroke="#02C2CE"
+          strokeWidth="2.08696"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_473_13042">
+          <rect
+            width="21.3333"
+            height="21.3333"
+            fill="white"
+            transform="translate(5.12 5.35059)"
+          />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+}
