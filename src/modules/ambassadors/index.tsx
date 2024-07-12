9a56b0f54019e7f089e265a85c3d19bbe153bb8b
Abdulsamad Ayoade <ayoadesamad@gmail.com>
2024-07-12 11:01:15 +0100

    chore: add ambassadors page

--- src/modules/ambassadors/index.tsx
+++ src/modules/ambassadors/index.tsx
@@ -16,6 +16,11 @@ import MagnifyingGlassIcon from "@/src/components/icons/MagnifyingGlassIcon";
 import ListIcon from "@/src/components/icons/ListIcon";
 import GridIcon from "@/src/components/icons/GridIcon";
 import UserCard from "@/src/components/common/UserCard";
+import Sheet from "@/src/components/core/sheet";
+import AmbassadorDetails from "./components/AmbassadorDetails";
+import AmbassadorForm from "./components/AmbassadorForm";
+import { handleApiSuccessResponse } from "@/src/store/api/helper";
+import CapsuleIcon from "@/src/components/icons/CapsuleIcon";
 import type { TData, TData2 } from "./types";
 
 const ambassadors: TData2["ambassador"][] = [
@@ -313,13 +318,13 @@ export default function Ambassadors() {
           <div className="flex items-center space-x-2">
             <Button
               variant="ghost"
-              // onClick={handleSheet.bind(null, "view", id)}
+              onClick={handleSheet.bind(null, "view", id)}
               className="h-auto p-0 text-wheels-primary hover:underline">
               View
             </Button>
             <Button
               variant="ghost"
-              // onClick={handleSheet.bind(null, "edit", id)}
+              onClick={handleSheet.bind(null, "edit", id)}
               className="h-auto p-0 text-wheels-primary hover:underline">
               Edit
             </Button>
@@ -340,126 +345,175 @@ export default function Ambassadors() {
     setData2({ ...data2, [key]: value });
   }
 
+  function handleExportAmbassadors() {
+    handleApiSuccessResponse({
+      message: "Data exported successfully",
+    });
+  }
+
+  function toggleSheet() {
+    setData2({ ...data2, openSheet: !data2.openSheet });
+  }
+
+  function handleSheet(sheetType: TData2["sheetType"], id?: string) {
+    let ambassador = ambassadors.find((amb) => amb.id === id);
+    console.log(ambassador);
+
+    if (sheetType === "edit" && !ambassador) {
+      handleApiSuccessResponse({
+        message: "Ambassador not found",
+      });
+
+      return;
+    }
+
+    if ((sheetType === "edit" || sheetType === "view") && ambassador) {
+      setData2({ ...data2, ambassador, openSheet: true, sheetType });
+      return;
+    }
+
+    setData2({ ...data2, openSheet: true, sheetType });
+  }
+
   return (
-    <section className="p-5">
-      <div className="mb-8 grid grid-cols-1 gap-y-5 sm:grid-cols-2 sm:gap-x-5 lg:grid-cols-4">
-        <StatCard
-          icon={<UserIcon className="text-white" />}
-          iconClass="bg-[#10B981]"
-          value={200}
-          description="Total Ambassadors"
-        />
-        <StatCard
-          icon={<UserIcon className="text-white" />}
-          iconClass="bg-[#F59E0B]"
-          value={548}
-          description="Total Rentals"
-        />
-        <StatCard
-          icon={<UserIcon className="text-white" />}
-          iconClass="bg-[#5654D1]"
-          value={123}
-          description="Customers Onboarded"
-        />
-        <StatCard
-          icon={<UserIcon className="text-white" />}
-          iconClass="bg-[#47A4E9]"
-          value="120 KgCO2"
-          description="Total Emission saved"
-        />
-      </div>
-
-      <div className="rounded-md bg-white py-5">
-        <div className="flex flex-col space-y-5 px-5 md:flex-row md:items-center md:justify-between md:space-x-3 md:space-y-0">
-          <div className="flex w-full sm:space-x-5 md:w-[44%] md:items-center xl:w-[65%]">
-            <div className="hidden items-center sm:flex">
-              {[
-                {
-                  icon: <ListIcon />,
-                  value: "list",
-                },
-                {
-                  icon: <GridIcon />,
-                  value: "grid",
-                },
-              ].map(({ icon, value }, index) => (
-                <button
-                  key={index}
-                  onClick={() =>
-                    handleDataChange("view", value as TData["view"])
-                  }
-                  className={`p-2 ${index === 0 ? "rounded-bl-sm rounded-tl-sm" : "rounded-br-sm rounded-tr-sm"} ${data.view === value ? "bg-wheels-primary text-white" : "bg-wheels-grey-2 text-wheels-grey-3"}`}>
-                  {icon}
-                </button>
-              ))}
-            </div>
-
-            <div className="relative w-full">
-              <MagnifyingGlassIcon className="absolute left-4 top-3" />
-              <input
-                type="text"
-                placeholder="Search"
-                className="h-[42px] w-full rounded-sm border border-wheels-grey-4 pl-10 pr-3 text-sm text-wheels-primary outline-none focus:border-wheels-primary"
-              />
-            </div>
-          </div>
-
-          <div className="flex items-center sm:space-x-3 md:w-[55%] md:justify-end xl:w-[35%]">
-            <Button
-              // onClick={handleExportAssets}
-              variant="ghost"
-              className="flex items-center space-x-2.5 rounded-lg border border-wheels-grey-4 px-4 py-2.5">
-              <SheetIcon />
-              <span className="text-sm font-medium text-wheels-primary">
-                Export
-              </span>
-            </Button>
-
-            <Button
-              // onClick={() => handleSheet("add")}
-              className="flex items-center space-x-2">
-              <PlusIcon />
-              <span className="text-sm font-medium">Add New</span>
-            </Button>
-          </div>
+    <>
+      <section className="p-5">
+        <div className="mb-8 grid grid-cols-1 gap-y-5 sm:grid-cols-2 sm:gap-x-5 lg:grid-cols-4">
+          <StatCard
+            icon={<UserIcon className="text-white" />}
+            iconClass="bg-[#10B981]"
+            value={200}
+            description="Total Ambassadors"
+          />
+          <StatCard
+            icon={<CapsuleIcon />}
+            iconClass="bg-[#F59E0B]"
+            value={548}
+            description="Total Rentals"
+          />
+          <StatCard
+            icon={<UserIcon className="text-white" />}
+            iconClass="bg-[#5654D1]"
+            value={123}
+            description="Customers Onboarded"
+          />
+          <StatCard
+            icon={<CapsuleIcon />}
+            iconClass="bg-[#47A4E9]"
+            value="120 KgCO2"
+            description="Total Emission saved"
+          />
         </div>
 
-        <div className="mt-5">
-          {data.view === "list" ? (
-            <DataTable
-              data={ambassadors}
-              columns={columns}
-              rowSelection={data2.rowSelection}
-              setRowSelection={(value: TData2["ambassador"]) =>
-                handleData2Change("rowSelection", value)
-              }
-            />
-          ) : (
-            <div className="grid grid-cols-1 gap-5 xs:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
-              {ambassadors.map((ambassador, index) => (
-                <UserCard
-                  key={index}
-                  address={ambassador.address}
-                  status={ambassador.status}
-                  name={ambassador.fullName}
-                  dateCreated={ambassador.createdAt}
+        <div className="rounded-md bg-white py-5">
+          <div className="flex flex-col space-y-5 px-5 md:flex-row md:items-center md:justify-between md:space-x-3 md:space-y-0">
+            <div className="flex w-full sm:space-x-5 md:w-[44%] md:items-center xl:w-[65%]">
+              <div className="hidden items-center sm:flex">
+                {[
+                  {
+                    icon: <ListIcon />,
+                    value: "list",
+                  },
+                  {
+                    icon: <GridIcon />,
+                    value: "grid",
+                  },
+                ].map(({ icon, value }, index) => (
+                  <button
+                    key={index}
+                    onClick={() =>
+                      handleDataChange("view", value as TData["view"])
+                    }
+                    className={`p-2 ${index === 0 ? "rounded-bl-sm rounded-tl-sm" : "rounded-br-sm rounded-tr-sm"} ${data.view === value ? "bg-wheels-primary text-white" : "bg-wheels-grey-2 text-wheels-grey-3"}`}>
+                    {icon}
+                  </button>
+                ))}
+              </div>
+
+              <div className="relative w-full">
+                <MagnifyingGlassIcon className="absolute left-4 top-3" />
+                <input
+                  type="text"
+                  placeholder="Search"
+                  className="h-[42px] w-full rounded-sm border border-wheels-grey-4 pl-10 pr-3 text-sm text-wheels-primary outline-none focus:border-wheels-primary"
                 />
-              ))}
+              </div>
             </div>
-          )}
-        </div>
 
-        <Pagination
-          className="px-5"
-          initialPage={data.page ? data.page - 1 : 0}
-          pageCount={10} // TODO - replace with actual data from api
-          totalDataLength={200} // TODO - replace with actual data from api
-          currentRange={{ start: 1, end: 10 }} // TODO - replace with actual data from api
-          onPageChange={(page) => {
-            handleDataChange("page", page + 1);
-          }}
-        />
-      </div>
-    </section>
+            <div className="flex items-center sm:space-x-3 md:w-[55%] md:justify-end xl:w-[35%]">
+              <Button
+                onClick={handleExportAmbassadors}
+                variant="ghost"
+                className="flex items-center space-x-2.5 rounded-lg border border-wheels-grey-4 px-4 py-2.5">
+                <SheetIcon />
+                <span className="text-sm font-medium text-wheels-primary">
+                  Export
+                </span>
+              </Button>
+
+              <Button
+                onClick={() => handleSheet("add")}
+                className="flex items-center space-x-2">
+                <PlusIcon />
+                <span className="text-sm font-medium">Add New</span>
+              </Button>
+            </div>
+          </div>
+
+          <div className="mt-5">
+            {data.view === "list" ? (
+              <DataTable
+                data={ambassadors}
+                columns={columns}
+                rowSelection={data2.rowSelection}
+                setRowSelection={(value: TData2["ambassador"]) =>
+                  handleData2Change("rowSelection", value)
+                }
+              />
+            ) : (
+              <div className="grid grid-cols-1 gap-5 xs:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
+                {ambassadors.map((ambassador, index) => (
+                  <UserCard
+                    key={index}
+                    address={ambassador.address}
+                    status={ambassador.status}
+                    name={ambassador.fullName}
+                    dateCreated={ambassador.createdAt}
+                  />
+                ))}
+              </div>
+            )}
+          </div>
+
+          <Pagination
+            className="px-5"
+            initialPage={data.page ? data.page - 1 : 0}
+            pageCount={10} // TODO - replace with actual data from api
+            totalDataLength={200} // TODO - replace with actual data from api
+            currentRange={{ start: 1, end: 10 }} // TODO - replace with actual data from api
+            onPageChange={(page) => {
+              handleDataChange("page", page + 1);
+            }}
+          />
+        </div>
+      </section>
+
+      <Sheet
+        open={data2.openSheet}
+        onOpenChange={() => handleData2Change("openSheet", !data2.openSheet)}
+        className="w-full max-w-full px-0 pb-0 md:max-w-[520px]"
+        showCloseButton>
+        {(data2.sheetType === "add" || data2.sheetType === "edit") && (
+          <AmbassadorForm
+            close={toggleSheet}
+            type={data2.sheetType}
+            ambassador={data2.ambassador}
+          />
+        )}
+        {data2.sheetType === "view" && (
+          <AmbassadorDetails ambassador={data2.ambassador} />
+        )}
+      </Sheet>
+    </>
   );
 }
