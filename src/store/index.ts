c79abc55a09ec4dd4fd60d44a4eee6de54e7063b
Abdulsamad Ayoade <ayoadesamad@gmail.com>
2024-07-18 14:26:59 +0100

    chore: add file api

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,17 +2,20 @@ import { Action, configureStore, ThunkAction } from "@reduxjs/toolkit";
 import authReducer from "./features/authSlice";
 import { authApi } from "./api/auth";
 import { ambassadorsApi } from "./api/ambassadors";
+import { fileApi } from "./api/file";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     [authApi.reducerPath]: authApi.reducer,
     [ambassadorsApi.reducerPath]: ambassadorsApi.reducer,
+    [fileApi.reducerPath]: fileApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(
       authApi.middleware,
       ambassadorsApi.middleware,
+      fileApi.middleware,
     ),
 });
 
